9d33c89d434131ae8da727ca3e21ea845e4f0f4f
tkumata <tkumata@gmail.com>
2017-12-31 10:33:28 +0900

    fix mplayer option for raspbian stretch

--- storage/play.sh
+++ storage/play.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-cmd='mplayer -noconsolecontrols -really-quiet -novideo -af volnorm=2:0.25'
+#cmd='mplayer -noconsolecontrols -really-quiet -novideo -af volnorm=2:0.25'
+cmd='mplayer -really-quiet -vo null'
 url=$1
 opt=$2
 
 if [ ! -z "$opt" ]; then
-    nohup $cmd "$url $opt" < /dev/null > /dev/null 2>&1 &
+    #nohup $cmd "$url $opt" < /dev/null > /dev/null 2>&1 &
+    nohup $cmd "$url" < /dev/null > /dev/null 2>&1 &
 else
     nohup $cmd "$url" < /dev/null > /dev/null 2>&1 &
 fi
